170f21eb428862b5866c3b5cc671a03ff5056b36
Ian Wright <iantwright13@gmail.com>
2017-11-09 18:29:57 -0500

    Updated packages and events
    Added:
         -deep-diff package
         -Ban logging
         -guildUpdates
         -guildMemberUpdates

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const ddiff = require('return-deep-diff');
 const client = new Discord.Client();
 const config = require('./config.json');
 
@@ -6,6 +7,7 @@ client.on('ready',() => {
   console.log("I'm online and ready to serve!");
 });
 
+//Guild Events
 client.on('guildDelete', guild => {
   console.log(`I have left ${guild.name} at ${new Date()}`);
 });
@@ -24,8 +26,41 @@ client.on('guildMemberRemove', member => {
   guild.channels.get(config.defaultChannel).send(`Goodbye, ${member.user.username}!`);
 });
 
+client.on('guildMemberUpdate', (oMember, nMember) => {
+  console.log(ddiff(oMember, nMember));
+});
+
+client.on('guildUpdate', (oGuild, nGuild) => {
+  console.log(ddiff(oGuild, nGuild));
+});
+
+client.on('guildBanAdd', (Guild, User) => {
+  guild.channels.get(config.defaultChannel).send(`${user.username} has been banned!`);
+});
+
+client.on('guildBanRemove', (Guild, User) => {
+  guild.channels.get(config.defaultChannel).send(`${user.username} has been unbanned!`);
+});
+
+//Client Events
+client.on('channelCreate'. channel => {
+  console.log(`A ${channel.type} channel called ${channel.name} was created at ${channel.createdAt} with the ID of ${channel.id}`);
+
+});
+
+client.on('channelDelete'. channel => {
+  console.log(`The ${channel.type} channel, ${channel.name} was successfully deleted.`);
+
+});
+
+client.on('channelUpdate', (oChannel, nChannel) => {
+  console.log(ddiff(oChannel, nChannel));
+});
+
+//Prefix
 var prefix = "?"
 
+//Command Handler
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   let args = message.content.split(' ').slice(1);
@@ -38,4 +73,6 @@ client.on('message', message => {
   }
 });
 
+
+//Discord Login
 client.login(config.token);
