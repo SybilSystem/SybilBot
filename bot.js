3baaeab0787ede7475796bd9ef6dd95112f26b07
Ian Wright <iantwright13@gmail.com>
2017-11-09 08:53:06 -0500

    Added new events and commands.
    Added:
         -guildCreate
         -guildDelete
         -guildMemberAdd
         -guildMemberDelete
         -"foobar" test command
    
    Modified:
         -"ping" command

--- bot.js
+++ bot.js
@@ -3,15 +3,38 @@ const client = new Discord.Client();
 const config = require('./config.json');
 
 client.on('ready',() => {
-  console.log("I'm Online");
-})
+  console.log("I'm online and ready to serve!");
+});
 
-var prefix = "-!"
+client.on('guildDelete', guild => {
+  console.log(`I have left ${guild.name} at ${new Date()}`);
+});
+
+client.on('guildCreate', guild => {
+  guild.channels.get(config.defaultChannel).send(`I have joined \`${guild.name}\``);
+});
+
+client.on('guildMemberAdd', member => {
+  let guild = member.guild;
+  guild.channels.get(config.defaultChannel).send(`Please welcome ${member.user.username} to the server!`);
+});
+
+client.on('guildMemberRemove', member => {
+  let guild = member.guild;
+  guild.channels.get(config.defaultChannel).send(`Goodbye, ${member.user.username}!`);
+});
+
+var prefix = "?"
 
 client.on('message', message => {
-  if (message.author === client.user) return;
+  if (!message.content.startsWith(prefix)) return;
+  let args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.bot) return;
+
+
   if (message.content.startsWith(prefix + 'ping')) {
-    message.reply('pong');
+    message.channel.send(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
   }
 });
 
