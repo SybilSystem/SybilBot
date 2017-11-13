d4ce55eac421ceb2e581d5a4ff60b2ae67bc1504
Ian Wright <iantwright13@gmail.com>
2017-11-13 10:31:43 -0500

    Updates to the command handler and logging
    v2.1.0

--- bot.js
+++ bot.js
@@ -4,25 +4,64 @@ if (process.version.slice(1).split(".")[0] < 8) throw new Error("Node 8.0.0 or h
 const Discord = require('discord.js');
 const ddiff = require('return-deep-diff');
 const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
 const client = new Discord.Client();
 const config = require('./config.json');
 require('./util/eventLoader.js')(client);
 
-//Reload
-var reload = (message, cmd) => {
-	delete require.cache[require.resolve('./commands/' + cmd)];
-	try {
-		let cmdFile = require('./commands/' + cmd);
-	} catch (err) {
-		message.channel.send(`Problem loading ${cmd}: ${err}`).then(
-			response => response.delete(1000).catch(error => console.log(error.stack))
-		).catch(error => console.log(error.stack));
-	}
-	message.channel.send(`${cmd} reload was a success!`).then(
-		response => response.delete(1000).catch(error => console.log(error.stack))
-	).catch(error => console.log(error.stack));
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
 };
-exports.reload = reload;
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(f => {
+    let props = require(`./commands/${f}`);
+    log(`Loading Command: ${props.help.name}. 👌`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      let cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  /* This function should resolve to an ELEVATION level which
+     is then sent to the command handler for verification*/
+  let permlvl = 0;
+  let mod_role = message.guild.roles.find('name', config.modrolename);
+  if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
+  let admin_role = message.guild.roles.find('name', config.adminrolename);
+  if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 3;
+  if (message.author.id === config.ownerid) permlvl = 4;
+  return permlvl;
+};
+
+
 
 //Debugging and Error logging
 var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g; //Redacts token in debug logging.
--- commands/foo.js
+++ commands/foo.js
@@ -1,3 +1,16 @@
-exports.run = function(client, message, args) {
+exports.run = function(client, message) {
   message.channel.send('bar');
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'foo',
+  description: 'Foo/Bar test command. Clearly for testing purposes and nothing else.',
+  usage: 'foo'
 };
